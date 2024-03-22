f1121ce4c3d11e1b0c91ee833b5ab8fef5174ea2
David Joy <davjoy@gmail.com>
2024-03-22 17:00:28 -0400

    feat: loading code from other MFEs
    Load a script, have it dump its exports in global, find them once it loads, pass them to react to render.  Done.

--- src/example/ExamplePage.jsx
+++ src/example/ExamplePage.jsx
@@ -1,12 +1,35 @@
 import { Container } from '@openedx/paragon';
+import { Suspense, lazy } from 'react';
 
-const ExamplePage = () => (
-  <main>
-    <Container className="py-5">
-      <h1>Example Page</h1>
-      <p>Hello world!</p>
-    </Container>
-  </main>
-);
+function loadRemoteComponent(src) {
+  return new Promise((resolve, reject) => {
+    const script = document.createElement('script');
 
+    script.type = 'text/javascript';
+    script.src = src;
+    script.onload = () => {
+      resolve({ default: global.ChildPage });
+    };
+    script.onerror = () => {
+      reject(new Error('Script loading error.'));
+    };
+    document.head.appendChild(script);
+  });
+}
+
+const ExamplePage = () => {
+  const ChildPage = lazy(() => loadRemoteComponent('http://localhost:8081/child.js'));
+
+  return (
+    <main>
+      <Container className="py-5">
+        <h1>I am the parent page.</h1>
+        <p>Hello, I am a parent!</p>
+        <Suspense fallback="Loading...">
+          <ChildPage />
+        </Suspense>
+      </Container>
+    </main>
+  );
+};
 export default ExamplePage;
--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,28 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-import {
-  APP_INIT_ERROR, APP_READY, subscribe, initialize,
-} from '@edx/frontend-platform';
-import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 import ReactDOM from 'react-dom';
+import React from 'react';
+
+import * as FrontendPlatform from '@edx/frontend-platform';
+import * as FrontendPlatformConfig from '@edx/frontend-platform/config';
+import { AppProvider, ErrorPage } from '@edx/frontend-platform/react';
 
 import Header from '@edx/frontend-component-header';
 import Footer from '@edx/frontend-component-footer';
+
 import messages from './i18n';
-import ExamplePage from './example/ExamplePage';
 
 import './index.scss';
+import ExamplePage from './example/ExamplePage';
+
+global.React = React;
+global.ReactDOM = ReactDOM;
+global.FrontendPlatform = FrontendPlatform;
+global.FrontendPlatformConfig = FrontendPlatformConfig;
+
+const {
+  APP_INIT_ERROR, APP_READY, subscribe, initialize,
+} = FrontendPlatform;
 
 subscribe(APP_READY, () => {
   ReactDOM.render(
@@ -30,5 +40,9 @@ subscribe(APP_INIT_ERROR, (error) => {
 });
 
 initialize({
+  requireAuthenticatedUser: false,
   messages,
+  handlers: {
+    auth: () => {}, // Turn off auth
+  },
 });
